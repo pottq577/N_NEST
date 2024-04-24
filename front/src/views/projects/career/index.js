82c9974cc906be22410a707dde8830f30a88d4a3
HWKIM <pottq577@naver.com>
2024-04-24 15:18:22 +0900

    경력관리 페이지 - 학력 완성

--- front/src/views/projects/career/index.js
+++ front/src/views/projects/career/index.js
@@ -3,13 +3,23 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import Button from '@mui/material/Button'
+import Select from '@mui/material/Select'
 import Divider from '@mui/material/Divider'
+import MenuItem from '@mui/material/MenuItem'
 import TextField from '@mui/material/TextField'
-import { styled } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
 import Typography from '@mui/material/Typography'
+import FormControl from '@mui/material/FormControl'
 
-// import EditIcon from '@mui/icons-material/Edit'
-// import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction'
+
+import { styled } from '@mui/material/styles'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import IconButton from '@mui/material/IconButton'
 
 const ImgStyled = styled('img')(({ theme }) => ({
   width: 100,
@@ -29,7 +39,7 @@ const ProfileSection = () => {
             flexDirection: 'column',
             alignItems: 'flex-start',
             justifyContent: 'center',
-            minHeight: 100
+            minHeight: 150
           }}
         >
           <Typography variant='h4'>홍길동</Typography>
@@ -38,11 +48,17 @@ const ProfileSection = () => {
         </Box>
       </Grid>
       <Grid item xs={3}>
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}
+        >
           <ImgStyled src={imgSrc} alt='프로필 사진' />
-          {/* <IconButton color='primary' aria-label='edit profile'>
+          <IconButton id='edit' color='primary' aria-label='edit profile'>
             <EditIcon />
-          </IconButton> */}
+          </IconButton>
         </Box>
       </Grid>
     </Grid>
@@ -50,64 +66,190 @@ const ProfileSection = () => {
 }
 
 const EducationSection = () => {
+  const [educationDetails, setEducationDetails] = useState({
+    type: '',
+    name: '',
+    major: '',
+    status: '',
+    startDate: null,
+    endDate: null
+  })
   const [educations, setEducations] = useState([])
-  const [newEducation, setNewEducation] = useState('')
   const [isAdding, setIsAdding] = useState(false)
 
-  const addEducation = () => {
+  const handleAddClick = () => {
     setIsAdding(true)
   }
 
-  const saveEducation = () => {
-    if (newEducation.trim()) {
-      setEducations([...educations, newEducation.trim()])
-    }
-    setNewEducation('')
-    setIsAdding(false)
+  const handleChange = e => {
+    const { name, value } = e.target
+    setEducationDetails({
+      ...educationDetails,
+      [name]: value
+    })
   }
 
-  const cancelAddEducation = () => {
-    setNewEducation('')
+  const saveEducation = () => {
+    setEducations([...educations, educationDetails])
     setIsAdding(false)
+    setEducationDetails({
+      type: '',
+      name: '',
+      major: '',
+      status: '',
+      startDate: null,
+      endDate: null
+    })
+    console.log(educationDetails)
+  }
+
+  const cancelEducation = () => {
+    setIsAdding(false)
+    setEducationDetails({
+      type: '',
+      name: '',
+      major: '',
+      status: '',
+      startDate: null,
+      endDate: null
+    })
+  }
+
+  const educationType = type => {
+    const types = {
+      college: '대학 (2,3년)',
+      university: '대학교 (4년)',
+      masters: '대학원(석사)',
+      phd: '대학원(박사)'
+    }
+
+    return types[type] || ''
+  }
+
+  const graduationStatus = status => {
+    const statuses = {
+      graduated: '졸업',
+      enrolled: '재학',
+      leaveOfAbsence: '휴학',
+      completion: '수료',
+      dropOut: '중퇴',
+      withdrawal: '자퇴',
+      expectedGraduation: '졸업예정'
+    }
+
+    return statuses[status] || ''
   }
 
   return (
-    <Box sx={{ width: '100%', minHeight: 150 }}>
+    <Box sx={{ width: '100%' }}>
       <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
         <Typography variant='h6'>학력</Typography>
-        {!isAdding && (
-          <Button variant='outlined' onClick={addEducation}>
-            추가
-          </Button>
-        )}
+        <Button variant='outlined' onClick={handleAddClick}>
+          추가
+        </Button>
       </Box>
-      <Divider sx={{ my: 3 }} />
-      <Box sx={{ mt: 2 }} style={{ padding: 10 }}>
-        {educations.map((education, index) => (
-          <Typography key={index} variant='body1'>
-            • {education}
-          </Typography>
-        ))}
+
+      <Divider sx={{ mb: 2 }} />
+
+      <Box style={{ minHeight: 100 }}>
         {isAdding && (
-          <>
-            <TextField
-              fullWidth
-              variant='outlined'
-              label='학력 추가'
-              value={newEducation}
-              onChange={e => setNewEducation(e.target.value)}
-              margin='normal'
-            />
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 1 }}>
-              <Button sx={{ mr: 1 }} variant='outlined' onClick={cancelAddEducation}>
-                취소
-              </Button>
-              <Button variant='contained' color='primary' onClick={saveEducation}>
+          <Grid container spacing={2}>
+            {/* 대학 구분 선택 */}
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+                <InputLabel id='type-label'>대학 구분</InputLabel>
+                <Select
+                  labelId='type-label'
+                  id='type'
+                  name='type'
+                  value={educationDetails.type}
+                  label='대학 구분'
+                  onChange={handleChange}
+                >
+                  <MenuItem value={'college'}>대학 (2,3년)</MenuItem>
+                  <MenuItem value={'university'}>대학교 (4년)</MenuItem>
+                  <MenuItem value={'masters'}>대학원(석사)</MenuItem>
+                  <MenuItem value={'phd'}>대학원(박사)</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            {/* 학교명 입력 */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id='name'
+                name='name'
+                label='학교명'
+                value={educationDetails.name}
+                onChange={handleChange}
+              />
+            </Grid>
+            {/* 전공 입력 */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id='major'
+                name='major'
+                label='전공'
+                value={educationDetails.major}
+                onChange={handleChange}
+              />
+            </Grid>
+            {/* 졸업 여부 */}
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+                <InputLabel id='status-label'>졸업 여부</InputLabel>
+                <Select
+                  labelId='status-label'
+                  id='status'
+                  name='status'
+                  value={educationDetails.status}
+                  label='졸업 여부'
+                  onChange={handleChange}
+                >
+                  <MenuItem value={'graduated'}>졸업</MenuItem>
+                  <MenuItem value={'enrolled'}>재학</MenuItem>
+                  <MenuItem value={'leaveOfAbsence'}>휴학</MenuItem>
+                  <MenuItem value={'completion'}>수료</MenuItem>
+                  <MenuItem value={'dropOut'}>중퇴</MenuItem>
+                  <MenuItem value={'withdrawal'}>자퇴</MenuItem>
+                  <MenuItem value={'expectedGraduation'}>졸업예정</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={{ mt: 12 }}>
+              <Button variant='contained' onClick={saveEducation}>
                 저장
               </Button>
-            </Box>
-          </>
+
+              <Button variant='contained' onClick={cancelEducation}>
+                취소
+              </Button>
+            </Grid>
+          </Grid>
         )}
+
+        <List style={{ justifyContent: 'flex-start' }}>
+          {educations.map((education, index) => (
+            <ListItem key={index}>
+              <ListItemText
+                primary={`${educationType(education.type)} | ${education.startDate} ~ ${
+                  education.endDate
+                } (${graduationStatus(education.status)})`}
+                secondary={`${education.name} / ${education.major}`}
+              />
+              <ListItemSecondaryAction>
+                <IconButton edge='end' aria-label='edit'>
+                  <EditIcon />
+                </IconButton>
+                <IconButton edge='end' aria-label='delete'>
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
       </Box>
     </Box>
   )
@@ -138,8 +280,8 @@ const Career = () => {
           <Box sx={{ my: 10 }} />
 
           {/* 스킬 섹션 */}
-          <Box sx={{ my: 10 }} />
           <EducationSection />
+          <Box sx={{ my: 10 }} />
         </Grid>
         {/* 우측 요약 정보 섹션 */}
         <Grid item xs={12} md={4}>
